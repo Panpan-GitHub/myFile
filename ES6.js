e2218b94ef53e297f094ca7b3150cd19a75593e4
Panpan-GitHub <574155661@qq.com>
2019-02-28 17:00:40 +0800

    2019年2月28日 17:00:39

--- ES6.js
+++ ES6.js
@@ -1,3 +1,32 @@
+1.变量的解构赋值
+let [a,b,b] = [1,2,3]
+let [a,[b],c] = [1,[2],3] //解构赋值要求两边模式相同
+
+let [x,y = 'a'] = ['b'] //解构赋值允许设置默认值
+
+//注意：ES6使用严格的（===）判断一个位置是否有效
+
+//如果默认值是一个表达式，那么这个表达式是惰性求值的，即只有在用到的时候，才会求值。
+function f() {
+  console.log('aaa');
+}
+
+let [x = f()] = [1];
+
+2.对象的解构赋值
+let {foo,baz} = {foo:"a",baz:"b"}
+
+嵌套解构
+let obj = {
+    p: [
+      'Hello',
+      { y: 'World' }
+    ]
+  };
+  
+  let { p: [x, { y }] } = obj;
+  如果要为P赋值
+  let {p,p:[x, { y }]} = obj
 3.数值的扩展
 Number.isFinite    //判断一个数值是否为有限的
 Number.isNaN    //判断一个数值是否为空
@@ -44,4 +73,40 @@ function add(...values) {
   const a2 = [...a1];   //克隆数组
   [...arr1, ...arr2, ...arr3]   //合并数组
   Array.from()  //可以将类似数组对象和可遍历对象转换为数组
-  
+  let arrayLike = {
+    '0': 'a',
+    '1': 'b',
+    '2': 'c',
+    length: 3
+};
+let arr2 = Array.from(arrayLike); // ['a', 'b', 'c']
+
+  Array.of(3,4,5)  //将一组值，转换为数组。
+  find()  //用于找出第一个符合条件的数组成员
+  fill()  //使用给定值，填充一个数组。
+  ['a', 'b', 'c'].fill(7, 1, 2)
+// ['a', 7, 'c']
+
+// ES6 提供三个新的方法——entries()，keys()和values()——用于遍历数组。
+// keys()是对键名的遍历、values()是对键值的遍历，entries()是对键值对的遍历。
+for (let index of ['a', 'b'].keys()) {
+  console.log(index);
+}
+// 0
+// 1
+
+for (let elem of ['a', 'b'].values()) {
+  console.log(elem);
+}
+// 'a'
+// 'b'
+
+for (let [index, elem] of ['a', 'b'].entries()) {
+  console.log(index, elem);
+}
+// 0 "a"
+// 1 "b"
+
+incluudes()   //方法返回一个布尔值，表示某个数组是否包含给定的值
+flat()  //用于将嵌套的数组“拉平”，变成一维的数组。该方法返回一个新数组，对原数据没有影响。
+flat(1) //表示拉平一层，Infinity关键字可以将所有层拉平
